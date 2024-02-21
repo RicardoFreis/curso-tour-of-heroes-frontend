bdbc979f2d16fee1fb970a5d129cefc5bf3545f9
RicardoFreis <ricreis71@gmail.com>
2024-02-21 17:21:36 -0300

    Criação da Listagem de Heróis

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,15 +1,22 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero.model';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrl: './heroes.component.scss'
+  styleUrl: './heroes.component.scss',
 })
 export class HeroesComponent {
   hero: Hero = {
     id: 1,
-    name: 'Wolverine'
+    name: 'Wolverine',
   };
 
+  heroes = HEROES;
+  selectedHero?: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
